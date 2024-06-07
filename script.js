7a038e57570e42553090351a656224267a4aacee
Anna N <anedovba@gmail.com>
2024-06-07 17:42:14 -0500

    upd commit

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const toggleButton = document.getElementById('theme-toggle');
-const body = document.body;
+        const body = document.body;
+        const icon = toggleButton.querySelector('i');
 
-toggleButton.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-    body.classList.toggle('light-mode');
-});
+        toggleButton.addEventListener('click', () => {
+            body.classList.toggle('dark-mode');
+            body.classList.toggle('light-mode');
+
+            if (body.classList.contains('dark-mode')) {
+                icon.classList.remove('fa-moon');
+                icon.classList.add('fa-sun');
+            } else {
+                icon.classList.remove('fa-sun');
+                icon.classList.add('fa-moon');
+            }
+        });
